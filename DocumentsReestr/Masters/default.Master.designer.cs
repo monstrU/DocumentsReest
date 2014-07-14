fc8959017194aeed7b9b12f61d9d8df8bda639e4
monstrU <hp1999@mail.ru>
2014-07-14 22:48:21 +0400

    хлебные крошки

--- DocumentsReestr/Masters/default.Master.designer.cs
+++ DocumentsReestr/Masters/default.Master.designer.cs
@@ -49,15 +49,6 @@ namespace DocumentsReestr.Masters {
         protected global::System.Web.UI.WebControls.Menu mnuMain;
         
         /// <summary>
-        /// smpMain control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.SiteMapPath smpMain;
-        
-        /// <summary>
         /// smmdsMain control.
         /// </summary>
         /// <remarks>
@@ -65,6 +56,15 @@ namespace DocumentsReestr.Masters {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.SiteMapDataSource smmdsMain;
+        
+        /// <summary>
+        /// smpMain control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SiteMapPath smpMain;
         
         /// <summary>
         /// phContent control.
