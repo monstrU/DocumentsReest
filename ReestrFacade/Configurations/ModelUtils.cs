3669cd050ed8f7f161b7baf6e617a71f77348692
monstrU <hp1999@mail.ru>
2014-07-16 17:03:54 +0400

    последняя версия базы

---
+++ ReestrFacade/Configurations/ModelUtils.cs
@@ -0,0 +1,17 @@
+﻿namespace ReestrFacade.Configurations
+{
+    using System;
+    using System.Configuration;
+
+    public class ModelUtils
+    {
+        public static string ConnectionString
+        {
+            get
+            {
+
+                return ConfigurationManager.ConnectionStrings["reestrdb"].ConnectionString;
+            }
+        }
+    }
+}
